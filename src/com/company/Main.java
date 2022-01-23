05641731845076b266d878c89dbf78ae9c9e6fe0
KamenskyM <kamensky_ml@gmail.com>
2022-01-23 19:53:19 +0300

    branch3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello World");
-    System.out.println("Second_commit");
-    System.out.println("Next Commit");
-    System.out.println("master");
-    System.out.println("master&branch2");
-    System.out.println("Third Commit");
-    System.out.println("branch2");    }
+    System.out.println("branch3");    }
 
 }
