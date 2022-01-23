d1126251eaba713d844f0df1f709c03ef2f4150f
KamenskyM <kamensky_ml@gmail.com>
2022-01-24 00:34:20 +0300

    test reset

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello World");
-    System.out.println("branch3");    }
+    System.out.println("branch3");
+    System.out.println("before reset");
+    }
 
 }
